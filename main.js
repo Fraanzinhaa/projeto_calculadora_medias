7ec3e8597abd7677434b765bc9e6a5223a55999e
Fraanzinhaa <francielle.eal@gmail.com>
2024-02-16 15:01:28 -0300

    fix: limitação de casas decimais

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ function atualizaTabela() {
 function atualizaMediaFinal() {
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2); //tofixed vai limitar as casas decimais
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
 
